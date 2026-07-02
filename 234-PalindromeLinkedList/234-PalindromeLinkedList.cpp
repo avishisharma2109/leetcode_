1b7a31ca32630fa1579aa75d65b458f0cb06a223
Avishi Sharma <avishi.sharma.210906@gmail.com>
2026-07-02 06:28:56 +0530

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
@@ -0,0 +1,50 @@
+// Last updated: 7/2/2026, 6:28:55 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL|| head->next==NULL){
+            return true;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        //find middle element
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* prev = NULL;
+        ListNode* curr = slow;
+
+        while(curr != NULL)//reverse second half
+        {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        ListNode* first=head;
+        ListNode* second=prev;
+
+        while(second != NULL)
+        {
+            if(first->val != second->val)
+                return false;
+
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
