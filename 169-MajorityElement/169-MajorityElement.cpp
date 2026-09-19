e2977f9ac925bbca7782e5875680d08000a28916
Avishi Sharma <avishi.sharma.210906@gmail.com>
2026-09-19 18:41:26 +0530

    File updated at 9/19/2026, 6:41:25 PM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,24 +1,24 @@
-// Last updated: 6/13/2026, 1:29:30 PM
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-                int candidate = 0;
-        int count = 0;
-
-        for(int num : nums) {
-
-            if(count == 0) {
-                candidate = num;
-            }
-
-            if(num == candidate) {
-                count++;
-            }
-            else {
-                count--;
-            }
-        }
-
-        return candidate;
-    }
-};
+// Last updated: 9/19/2026, 6:41:25 PM
+1class Solution {
+2public:
+3    int majorityElement(vector<int>& nums) {
+4                int candidate = 0;
+5        int count = 0;
+6
+7        for(int num : nums) {
+8
+9            if(count == 0) {
+10                candidate = num;
+11            }
+12
+13            if(num == candidate) {
+14                count++;
+15            }
+16            else {
+17                count--;
+18            }
+19        }
+20
+21        return candidate;
+22    }
+23};
