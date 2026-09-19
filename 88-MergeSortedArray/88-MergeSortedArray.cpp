52787567d82a80be19416f30f34ae6d4ee844192
Avishi Sharma <avishi.sharma.210906@gmail.com>
2026-09-19 20:35:16 +0530

    File updated at 9/19/2026, 8:35:16 PM

--- 88-MergeSortedArray/88-MergeSortedArray.cpp
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -1,13 +1,13 @@
-// Last updated: 6/1/2026, 11:44:39 AM
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-class Solution {
-public:
-    void merge(vector<int>& v, int n, vector<int>& v1, int m) {
-        v.resize(n);
-        v.insert(v.end(), v1.begin(), v1.end());
-        sort(v.begin(), v.end());
-    }
-};
+// Last updated: 9/19/2026, 8:35:16 PM
+1#include <vector>
+2#include <algorithm>
+3using namespace std;
+4
+5class Solution {
+6public:
+7    void merge(vector<int>& v, int n, vector<int>& v1, int m) {
+8        v.resize(n);
+9        v.insert(v.end(), v1.begin(), v1.end());
+10        sort(v.begin(), v.end());
+11    }
+12};
