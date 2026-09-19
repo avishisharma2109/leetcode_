861e3f10231cea8abd843bc996f735e88398c336
Avishi Sharma <avishi.sharma.210906@gmail.com>
2026-09-20 00:57:49 +0530

    File updated at 9/20/2026, 12:57:49 AM

--- 88-MergeSortedArray/88-MergeSortedArray.cpp
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -1,13 +1,24 @@
-// Last updated: 9/19/2026, 8:35:16 PM
+// Last updated: 9/20/2026, 12:57:49 AM
 1#include <vector>
-2#include <algorithm>
-3using namespace std;
-4
-5class Solution {
-6public:
-7    void merge(vector<int>& v, int n, vector<int>& v1, int m) {
-8        v.resize(n);
-9        v.insert(v.end(), v1.begin(), v1.end());
-10        sort(v.begin(), v.end());
-11    }
-12};
+2using namespace std;
+3
+4class Solution {
+5public:
+6    int maxProfit(vector<int>& prices) {
+7        int minPrice = prices[0];
+8        int maxProfit = 0;
+9
+10        for (int i = 1; i < prices.size(); i++) {
+11            if (prices[i] < minPrice) {
+12                minPrice = prices[i];  
+13            } else {
+14                int profit = prices[i] - minPrice;
+15                if (profit > maxProfit) {
+16                    maxProfit = profit;
+17                }
+18            }
+19        }
+20
+21        return maxProfit;
+22    }
+23};
